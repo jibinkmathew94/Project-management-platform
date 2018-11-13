dc41bc8548f76f7f479ea25a428be1418edf630b
Jibin K Mathew <jibin.mathew94@gmail.com>
2018-11-13 14:10:41 +0530

    Created forms for Customer(Client) Creation,Project Creation,Feature Creation and templates for Customer Creation and Project Creation

--- flask_app/models.py
+++ flask_app/models.py
@@ -10,11 +10,19 @@ class Employee(db.Model, UserMixin):
 	feature_id = db.Column(db.Integer, db.ForeignKey('feature.id'),nullable=True)
 	feature = db.relationship("Feature",backref="assigned_members")
 
+	def __repr__(self):
+		return f'Employee {self.name}, Emp_id : {self.emp_id} email : {self.email}'
+
+
 
 class Customer(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(30), unique=False, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
+
+	def __repr__(self):
+		return f'Customer {self.name},email : {self.email}'
+
 
 
 class Project(db.Model):
@@ -23,6 +31,9 @@ class Project(db.Model):
 	description = db.Column(db.Text, nullable=False)
 	customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
 	customer = db.relationship("Customer",backref="Projects")
+
+	def __repr__(self):
+		return f'Project {self.title}'
 
 
 
@@ -33,6 +44,8 @@ class Feature(db.Model):
 	target_date = db.Column(db.DateTime, nullable=False)
 	project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
 	project = db.relationship("Project",backref="features")
+	def __repr__(self):
+		return f'Feature {self.title},email : {self.project}'
 
 
 
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,12 +1,40 @@
-from flask_app import app
-from flask import render_template
+from flask_app import app, db
+from flask import request
+from flask import render_template,redirect, url_for
+from flask_app.models import Customer, Project
+from flask_app.forms import RegistrationForm, LoginForm, CreateCustomerForm, CreateProjectForm, CreateFeatureForm
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template('base_layout.html')
+	return render_template('layout.html')
 
 
-@app.route("/about")
-def about():
-	return("about")
+
+@app.route("/create/customer",methods=['GET', 'POST'])
+def create_customer():
+	form = CreateCustomerForm()
+	if form.validate_on_submit():
+		customer = Customer(name=form.name.data,email=form.email.data)
+		db.session.add(customer)
+		db.session.commit()
+		return redirect(url_for('home'))
+	return render_template('register.html',form=form)
+
+
+@app.route("/create/project",methods=['GET','POST'])
+def create_project():
+	form = CreateProjectForm()
+	form.customer.choices=[(customer.id,customer.name) for customer in Customer.query.all()]
+	if form.validate_on_submit():
+		print(request.content_type)
+		return redirect(url_for('home'))
+	return render_template("create_project.html",form=form)
+
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+
